bfaecbfe9e6f0e6110e9510897d57afd651cb19d
Rafael <68511324+GalassoX@users.noreply.github.com>
2024-01-21 20:20:01 -0500

    initial code

---
+++ src/test/java/com/galasso/workshop/WorkshopApplicationTests.java
@@ -0,0 +1,13 @@
+package com.galasso.workshop;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WorkshopApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
